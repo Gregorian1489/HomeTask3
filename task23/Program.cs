9f53f67602949add31eff21ee998f59e424933e7
Gregorian1489 <santijago@mail.ru>
2022-11-21 16:53:06 +0400

    ADD task23

---
+++ task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int z;
+if (number>0)
+{
+for (int x = 1; x<=number;x++)
+{
+    Console.Write(z = x*x*x);
+    Console.Write(" ");
+}
+}
+else if (number<0)
+for (int x = -1; x>=number;x--)
+{
+    Console.Write(z = x*x*x);
+    Console.Write(" ");
+}
+else if (number==0) Console.WriteLine("Введите число отличное от нуля");
+
+
+
