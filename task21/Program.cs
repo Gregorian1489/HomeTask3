a7e43409d9b27c3f18515e117d969e8dc96580fa
Gregorian1489 <santijago@mail.ru>
2022-11-21 16:32:15 +0400

    ADD task21

---
+++ task21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координату x первой точки:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y первой точки:");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z первой точки:");
+int z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату x второй точки:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y второй точки:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату z третьей точки:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+double result;
+
+result =Math.Round(Math.Sqrt(Math.Pow(x1-x, 2) + Math.Pow(y1 - y, 2)+Math.Pow(z1 - z, 2)),2);
+
+Console.WriteLine($"Длина отрезка = {result}");
